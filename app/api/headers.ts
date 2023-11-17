e671bfd8eef045f33e7d230a453a7d91997051d7
ZakharovVladyslav <vladislavozakharov@gmail.com>
2023-11-17 20:01:58 +0200

    fix: caching problem resolved

--- app/api/headers.ts
+++ app/api/headers.ts
@@ -1,10 +1,12 @@
 export const authHeaders = {
     Authorization: `2502f5d4-2032-4e5c-80b3-80addf6c0820`,
     "X-RE-MV": "vladislavz@wayfarersolutions.com",
+    "Cache-Control": "no-cache",
 };
 
 export const postHeaders = {
     Authorization: `2502f5d4-2032-4e5c-80b3-80addf6c0820`,
     "X-RE-MV": "vladislavz@wayfarersolutions.com",
     "Content-type": "application/json",
+    "Cache-Control": "no-cache",
 };
